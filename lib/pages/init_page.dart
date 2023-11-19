7260a1e491ece8a21f7340a10f22d939b57e1f1d
kevinRojas13 <202000700b@utea.edu.pe>
2023-11-19 14:23:39 -0500

    select.colorss

--- lib/pages/init_page.dart
+++ lib/pages/init_page.dart
@@ -9,11 +9,24 @@ class InitPage extends StatefulWidget {
 
 class _InitPageState extends State<InitPage> {
   int _currentIndex = 0;
-
+  List<Widget> _pages = [
+    Center(child: Text("Principal")),
+    Center(child: Text("Shorts")),
+    Center(child: Text("Agregar")),
+    Center(child: Text("Suscripcion")),
+    Center(child: Text("Biblioteca")),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xff212121),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        selectedFontSize: 12.0,
+        unselectedFontSize: 12.0,
+
         type: BottomNavigationBarType.fixed,
         currentIndex: _currentIndex, // Corregir aquí
         onTap: (int value) {

61fcdfd7f7cd496fe9f2701997dbe5f16661ed66
kevinRojas13 <202000700b@utea.edu.pe>
2023-11-19 21:05:52 -0500

    culminando home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,9 +7,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
     return SingleChildScrollView(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 14, vertical: 4),
+        padding: EdgeInsets.symmetric(horizontal: 14, vertical: 0),
         child: Column(
           children: [
             SingleChildScrollView(
@@ -52,6 +54,68 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    Image.network(
+                      "https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                      width: double.infinity,
+                      height: height * 0.4,
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 5.0, vertical: 2.0),
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 10.0),
+                        color: Colors.black.withOpacity(0.7),
+                        child: Text(
+                          "23:22",
+                          style: TextStyle(color: Colors.white, fontSize: 12.0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.white12,
+                    backgroundImage: NetworkImage(
+                        "https://images.pexels.com/photos/1229356/pexels-photo-1229356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1,"),
+                  ),
+                  title: Text(
+                    "Lorem ipsum dolor sit amet",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "kevinFitness 6.5 M de vistas hace 4 años",
+                    style: TextStyle(
+                      color: Colors.white54,
+                      fontSize: 13.0,
+                    ),
+                  ),
+                  trailing: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 4.0),
+                        child: const Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
